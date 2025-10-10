3a8d2d862a87f401fe5d1167d7649e98d71a2c1c
mqqqqj <moqiji@outlook.com>
2025-10-10 17:24:11 +0800

    modify bash

--- scripts/parallel_search.sh
+++ scripts/parallel_search.sh
@@ -1,7 +1,7 @@
 cd build && make -j && cd ..
 thread_num=8
 
-L_list=(10 20 30 40 50 60 70 80 90 100)
+L_list=(40 50 60 70 80 90 100)
 
 # laion
 # L_str=$(IFS=,; echo "${L_list[*]}")
@@ -18,11 +18,11 @@ L_list=(10 20 30 40 50 60 70 80 90 100)
 # "$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_8t_ours.csv
 
 #mainsearch
-# L_str=$(IFS=,; echo "${L_list[*]}")
-# taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
-# /SSD/MainSearch/query_test_unique.fbin \
-# /SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
-# "$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_8t_ours.csv
+L_str=$(IFS=,; echo "${L_list[*]}")
+taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
+/SSD/MainSearch/query_test_unique.fbin \
+/SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
+"$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_8t_ours.csv
 
 #text-to-image
 # L_str=$(IFS=,; echo "${L_list[*]}")
@@ -39,11 +39,11 @@ L_list=(10 20 30 40 50 60 70 80 90 100)
 # "$L_str" 100 "$thread_num" /SSD/WebVid/gt.query.top100.bin "webvid" | tee -a /home/mqj/proj/ANNSLib/experiments/webvid_8t_ours.csv
 
 #GIST
-L_str=$(IFS=,; echo "${L_list[*]}")
-taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/GIST1M/base.fbin \
-/SSD/GIST1M/query.fbin \
-/SSD/models/nsg/gist1m.L2000.R64.C200.nsg \
-"$L_str" 100 "$thread_num" /SSD/GIST1M/gt.query.top100.bin "gist" | tee -a /home/mqj/proj/ANNSLib/experiments/gist_8t_ours.csv
+# L_str=$(IFS=,; echo "${L_list[*]}")
+# taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/GIST1M/base.fbin \
+# /SSD/GIST1M/query.fbin \
+# /SSD/models/nsg/gist1m.L2000.R64.C200.nsg \
+# "$L_str" 100 "$thread_num" /SSD/GIST1M/gt.query.top100.bin "gist" | tee -a /home/mqj/proj/ANNSLib/experiments/gist_8t_ours.csv
 
 #openai
 # L_list=(100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600)
