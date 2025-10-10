017d403ef8cf6e738a3ce1d6c308a52a9b49a3e5
mqqqqj <moqiji@outlook.com>
2025-10-10 22:49:59 +0800

    modify bash

--- scripts/edge_wise_search.sh
+++ scripts/edge_wise_search.sh
@@ -10,7 +10,7 @@ L_str=$(IFS=,; echo "${L_list[*]}")
 taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/LAION/LAION_base_imgemb_10M.fbin \
 /SSD/LAION/LAION_test_query_textemb_50k.fbin \
 /SSD/models/nsg/laion.L2000.R64.C2000.nsg \
-"$L_str" 100 "$thread_num" /SSD/LAION/gt.test50K.bin "laion" | tee -a /home/mqj/proj/ANNSLib/experiments/laion_8t_edgewise.csv
+"$L_str" 100 "$thread_num" /SSD/LAION/gt.test50K.bin "laion" | tee -a /home/mqj/proj/ANNSLib/experiments/laion_16t_edgewise.csv
 
 #glove
 L_list=(100 200 300 350 400 450 500 550 600 800 1000 1100 1200 1300 1400 1500 1600 1800 1900 2100 2300 2500 2700 2900 3000 3200 3400 3600 3800 4000 4200)
@@ -18,7 +18,7 @@ L_str=$(IFS=,; echo "${L_list[*]}")
 taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Glove200/base.1m.fbin \
 /SSD/Glove200/query.fbin \
 /SSD/models/nsg/glove200.L2000.R64.C200.nsg \
-"$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_8t_edgewise.csv
+"$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_16t_edgewise.csv
 
 #mainsearch
 L_list=(100 200 300 400 500 700 900 1100 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400 3600 3800 4000 4200)
@@ -26,7 +26,7 @@ L_str=$(IFS=,; echo "${L_list[*]}")
 taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
 /SSD/MainSearch/query_test_unique.fbin \
 /SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
-"$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_8t_edgewise.csv
+"$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_16t_edgewise.csv
 
 #text-to-image
 L_list=(100 200 300 400 500 700 900 1100 1400 1600 1800 2000 2200 2400 2600 2800 3000 3200 3400 3600 3800 4000 4200)
@@ -34,7 +34,7 @@ L_str=$(IFS=,; echo "${L_list[*]}")
 taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Text-to-Image/base.10M.fbin \
 /SSD/Text-to-Image/query.10k.fbin \
 /SSD/models/nsg/t2i10m.L2000.R64.C2000.nsg \
-"$L_str" 100 "$thread_num" /SSD/Text-to-Image/gt.10K_10M.bin "t2i" | tee -a /home/mqj/proj/ANNSLib/experiments/t2i_8t_edgewise.csv
+"$L_str" 100 "$thread_num" /SSD/Text-to-Image/gt.10K_10M.bin "t2i" | tee -a /home/mqj/proj/ANNSLib/experiments/t2i_16t_edgewise.csv
 
 # WebVid
 L_list=(100 200 300 350 400 450 500 550 600 800 1000 1100 1200 1300 1400 1500 1600 1800 1900 2100 2300 2500 2700 2900 3000 3200 3400 3600 3800 4000 4200)
@@ -42,7 +42,7 @@ L_str=$(IFS=,; echo "${L_list[*]}")
 taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/WebVid/webvid.base.2.5M.fbin \
 /SSD/WebVid/webvid.query.10k.fbin \
 /SSD/models/nsg/webvid.L2000.R64.C2000.nsg \
-"$L_str" 100 "$thread_num" /SSD/WebVid/gt.query.top100.bin "webvid" | tee -a /home/mqj/proj/ANNSLib/experiments/webvid_8t_edgewise.csv
+"$L_str" 100 "$thread_num" /SSD/WebVid/gt.query.top100.bin "webvid" | tee -a /home/mqj/proj/ANNSLib/experiments/webvid_16t_edgewise.csv
 
 #GIST
 # L_list=(100 125 150 175 200 225 250 275)
