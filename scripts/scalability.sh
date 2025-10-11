58629e0ba86ebae5a4585cdd793a1150e257657d
mqqqqj <moqiji@outlook.com>
2025-10-11 15:06:17 +0800

    a

--- scripts/scalability.sh
+++ scripts/scalability.sh
@@ -1,6 +1,6 @@
 cd build && make -j && cd ..
 
-thread_num=32
+thread_num=4
 
 # L_list=(100 150 200)
 
@@ -11,9 +11,16 @@ thread_num=32
 # "$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_32t_ours.csv
 
 
-L_list=(22 24 26 28)
+# L_list=(10 50)
+# L_str=$(IFS=,; echo "${L_list[*]}")
+# taskset -c 0-17,36-49 ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
+# /SSD/MainSearch/query_test_unique.fbin \
+# /SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
+# "$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_32t_ours.csv
+
+L_list=(100 110 120)
 L_str=$(IFS=,; echo "${L_list[*]}")
-taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
-/SSD/MainSearch/query_test_unique.fbin \
-/SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
-"$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_32t_ours.csv
+taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Text-to-Image/base.10M.fbin \
+/SSD/Text-to-Image/query.10k.fbin \
+/SSD/models/nsg/t2i10m.L2000.R64.C2000.nsg \
+"$L_str" 100 "$thread_num" /SSD/Text-to-Image/gt.10K_10M.bin "t2i" | tee -a /home/mqj/proj/ANNSLib/experiments/t2i_4t_ours.csv
