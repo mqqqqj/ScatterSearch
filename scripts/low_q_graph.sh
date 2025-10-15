59995d4ff7a9c117c6d26d452e6da3b68e673493
mqqqqj <moqiji@outlook.com>
2025-10-15 18:10:18 +0800

    low q graph search

--- scripts/low_q_graph.sh
+++ scripts/low_q_graph.sh
@@ -2,24 +2,27 @@ cd build && make -j && cd ..
 
 thread_num=8
 
-# L_list=(100 150 200 250 300 350 375 400 425 450 475 500 525 550 575 600 650 700 750 800 900 1000 1100 1200) #glove
-L_list=(2200 2400 2600) #glove
-L_str=$(IFS=,; echo "${L_list[*]}")
-taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Glove200/base.1m.fbin \
-/SSD/Glove200/query.fbin \
-/SSD/models/nsg/glove200.L500.R32.C500.nsg \
-"$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/glove_8t_ours.csv
-
-# L_list=(100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600 650 700 750 800 900 1000 1100 1200) #mainsearch
+# L_list=(100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2200 2400 2600 2800 3000) #glove
 # L_str=$(IFS=,; echo "${L_list[*]}")
-# taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/MainSearch/base.fbin \
-# /SSD/MainSearch/query_test_unique.fbin \
-# /SSD/models/nsg/mainsearch.L500.R32.C500.nsg \
-# "$L_str" 100 "$thread_num" /SSD/MainSearch/gt.test_unique.bin "mainsearch" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/mainsearch_8t_ours.csv
+# taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Glove200/base.1m.fbin \
+# /SSD/Glove200/query.fbin \
+# /SSD/models/nsg/glove200.L128.R16.C128.nsg \
+# "$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/glove_8t_ours_l128.csv
+
 
 # L_list=(100 150 200 250 300 350 400 450 500 550 600 700 800 900 1000 1200 1400 1600)
+# L_list=(1800 2000 2200 2400 2600 2800)
 # L_str=$(IFS=,; echo "${L_list[*]}")
 # taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/LAION/LAION_base_imgemb_10M.fbin \
 # /SSD/LAION/LAION_test_query_textemb_50k.fbin \
-# /SSD/models/nsg/laion.L500.R32.C500.nsg \
-# "$L_str" 100 "$thread_num" /SSD/LAION/gt.test50K.bin "laion" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/laion_8t_ours.csv
+# /SSD/models/nsg/laion.L200.R32.C200.nsg \
+# "$L_str" 100 "$thread_num" /SSD/LAION/gt.test50K.bin "laion" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/laion_8t_ours_l200.csv
+
+# L_list=(100 150 200 250 300 350 400 450 500 550 600 700 800 900 1000 1200 1400 1600 1800 2000 2200 2400 2600 2800) # WebVid
+L_list=(3300 3400 3500 3600) # WebVid
+
+L_str=$(IFS=,; echo "${L_list[*]}")
+taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/WebVid/webvid.base.2.5M.fbin \
+/SSD/WebVid/webvid.query.10k.fbin \
+/SSD/models/nsg/webvid.L200.R32.C200.nsg \
+"$L_str" 100 "$thread_num" /SSD/WebVid/gt.query.top100.bin "webvid" | tee -a /home/mqj/proj/ANNSLib/experiments/low_q_graph/webvid_8t_ours_l200.csv
