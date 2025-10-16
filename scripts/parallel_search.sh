9f7451c61d6c038b98ca3dce7a7d30d13b23d352
mqqqqj <moqiji@outlook.com>
2025-10-16 14:58:22 +0800

    a

--- scripts/parallel_search.sh
+++ scripts/parallel_search.sh
@@ -1,6 +1,6 @@
 cd build && make -j && cd ..
 
-thread_num=8
+thread_num=32
 
 # L_list=(100 150 200 250 300 350 400 450 500 550 600 700 800 900 1000 1200 1400 1600) # laion
 # L_str=$(IFS=,; echo "${L_list[*]}")
@@ -10,13 +10,13 @@ thread_num=8
 # "$L_str" 100 "$thread_num" /SSD/LAION/gt.test50K.bin "laion" | tee -a /home/mqj/proj/ANNSLib/experiments/ablation/laion_8t_ours_ws.csv
 
 
-L_list=(100 150 200 250 300 350 375 400 425 450 475 500 525 550 575 600 650 700 750 800 900 1000 1100 1200) #glove
+L_list=(160 165 170 175 180) #glove
 
 L_str=$(IFS=,; echo "${L_list[*]}")
-taskset -c 0-$((thread_num - 1)) ./build/tests/parallel_search /SSD/Glove200/base.1m.fbin \
+taskset -c 0-17,36-49 ./build/tests/parallel_search /SSD/Glove200/base.1m.fbin \
 /SSD/Glove200/query.fbin \
 /SSD/models/nsg/glove200.L2000.R64.C200.nsg \
-"$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/ablation/glove_8t_ours_ws.csv
+"$L_str" 100 "$thread_num" /SSD/Glove200/gt.query.top100.bin "glove" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_32t_ours.csv
 
 # L_list=(100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600 650 700 750 800 900 1000 1100 1200) #mainsearch
 
